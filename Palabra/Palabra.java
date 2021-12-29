a7f75967e0b64aac170943381e0af3baea050d03
rLB2019 <96724098+rLB2019@users.noreply.github.com>
2021-12-29 12:48:26 +0100

    Update Palabra.java

--- Palabra/Palabra.java
+++ Palabra/Palabra.java
@@ -15,6 +15,8 @@ public class Palabra implements Serializable {
     private boolean acertada;
     private boolean fallada;
     private boolean aplazada;
+    private boolean pistaDefinicion;
+    private boolean pistaLetras;
     private ArrayList<Definicion> definiciones;
 
     public Palabra(char i, String p, boolean acertada, boolean f, boolean aplazada, Definicion definicion) {
@@ -23,7 +25,9 @@ public class Palabra implements Serializable {
         this.acertada = acertada;
         this.fallada = f;
         this.aplazada = aplazada;
-        definiciones.add(definicion);
+        this.definiciones.add(definicion);
+        this.pistaDefinicion = false;
+        this.pistaLetras = false;
     }
 
     public String getPalabra() {
@@ -78,13 +82,25 @@ public class Palabra implements Serializable {
         }
         return null;
     }
+    
+    public int hayPista(){
+        if(pistaDefiniciones && pistaLetras) return 2;
+        if(pistaDefiniciones || pistaLetras) return 1;
 
-    public String PedirAyuda(int tipo, Jugador j) {
-        if (tipo == 1) {
-            return definiciones.get(1);
-        } else if (tipo == 0) {
+    public String pedirAyuda(int tipo, Jugador j) {
+        if (tipo == 1) {//Damos la definicion 2
+            pistaDefinicion = true;
+            return definiciones.get(1).getDefinicion();
+        } else if (tipo == 0) {//Damos letras desordenadas de la palabra
             //Hay que crear un metodo para crear la pista
+            pistaLetras = true;
+            //return X; //Devolvemos las letras en String
         }
         j.restarPuntos(5);
     }
+    
+    public String comprarPalabra(Jugador j){
+        j.restarPuntos(10);
+        return palabra;
+    }    
 }
