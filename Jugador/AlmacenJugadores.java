e5ebdf316ba8a37058c2f98b4f8edb36301a2f9b
rLB2019 <96724098+rLB2019@users.noreply.github.com>
2021-12-30 12:11:35 +0100

    Add files via upload

---
+++ Jugador/AlmacenJugadores.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jugador;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class AlmacenJugadores implements Serializable {
+    private final ArrayList<Jugador> jugadores; // HashSet
+    
+    public AlmacenJugadores() {
+        this.jugadores = new ArrayList<>();
+        this.jugadores.add(new Jugador());
+    }
+    
+    public ArrayList<Jugador> getJugadores() {
+        return this.jugadores;
+    }
+    
+    public Jugador jugadorConNombre(String nombre) {
+        Jugador jugador = null;
+        boolean encontrado = false;
+        Iterator<Jugador> it = this.jugadores.iterator();
+        while (!encontrado && it.hasNext()) {
+            jugador = it.next();
+            encontrado = jugador.getNombre().equalsIgnoreCase(nombre);
+        }
+        if (encontrado) {
+            return jugador;
+        } else {
+            return null;
+        }
+    }
+    
+    private boolean contieneJugadorConNombre(String nombre) {
+        boolean encontrado = false;
+        Iterator<Jugador> it = this.jugadores.iterator();
+        while (!encontrado && it.hasNext()) {
+            Jugador usuario = it.next();
+            encontrado = usuario.getNombre().equalsIgnoreCase(nombre);
+        return encontrado;
+    }
+    
+        
+    //Este metodo da illegal start of expression
+    public Jugador getJugador(String nombre, String clave){
+        
+        Jugador jugador = null;
+        
+        boolean encontrado = false;
+        Iterator<Jugador> it = getJugadores().iterator();
+        while (!encontrado && it.hasNext()) {
+            jugador = it.next();
+            encontrado = jugador.esLoginCorrecto(nombre, clave);
+        }
+        
+        if (!encontrado) {
+            jugador = null;
+        }
+        
+        return jugador;
+    }
+    
+    public Jugador agregarUsuario(String nombre, String clave) {
+        Jugador jugador = null;
+        if (!contieneJugadorConNombre(nombre)) {
+            jugador = new Jugador(nombre, clave);
+            this.jugadores.add(jugador);
+        }
+        return jugador;
+    }
+}
